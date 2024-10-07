e1740de06ebe0b93ded1e6b8a46a9079e9649a3e
Peti <peteru1963@gmail.com>
2024-10-07 10:26:06 +0200

    Finished dynamic temperature feature

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ async function start() {
     const weatherData = await weatherPromise.json();
 
     const ourTemperature = weatherData.properties.periods[0].temperature;
-console.log(ourTemperature);
+    document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
 start();
